c9d4464ea812d2bc98d6bedef40ba04d80f0e378
Safarmurod999 <supermen0904@gmail.com>
2024-01-11 18:50:04 +0500

    main

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app id="inspire">
+    <SidebarVue :drawer="drawer" />
+    <v-main>
+      <HeaderVue @handleDrawer="drawer = !drawer" />
+      <v-container class="py-8 px-6" fluid>
+        <v-row>
+          <v-col v-for="card in cards" :key="card" cols="12">
+            <v-card>
+              <v-list lines="two">
+                <v-list-subheader :title="card"></v-list-subheader>
+
+                <template v-for="n in 6" :key="n">
+                  <v-list-item>
+                    <template v-slot:prepend>
+                      <v-avatar color="grey-darken-1"></v-avatar>
+                    </template>
+
+                    <v-list-item-title
+                      :title="`Message ${n}`"
+                    ></v-list-item-title>
+
+                    <v-list-item-subtitle
+                      title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique"
+                    ></v-list-item-subtitle>
+                  </v-list-item>
+
+                  <v-divider
+                    v-if="n !== 6"
+                    :key="`divider-${n}`"
+                    inset
+                  ></v-divider>
+                </template>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import HeaderVue from "./components/HeaderVue.vue";
+import SidebarVue from "./components/SidebarVue.vue";
+const cards = ["Today", "Yesterday"];
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
---
+++ src/components/SidebarVue.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-navigation-drawer v-model="getDrawer" app>
+    <v-sheet color="grey-lighten-4" class="pa-4 text-center">
+      <v-avatar class="mb-4" color="grey-darken-1" size="64"></v-avatar>
+      <div>Dashboard</div>
+    </v-sheet>
+    <v-divider></v-divider>
+    <v-list>
+      <v-list-item
+        v-for="[icon, text] in links"
+        :key="icon"
+        :prepend-icon="icon"
+        :title="text"
+        link
+      ></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script setup>
+const links = [
+  ["mdi-view-dashboard", "Dashboard"],
+  ["mdi-account", "Profile"],
+  ["mdi-clipboard-list-outline", "Products"],
+  ["mdi-clipboard-list-outline", "Order"],
+  ["mdi-cogs", "Settings"],
+];
+</script>
+<script>
+export default {
+  props:{
+    drawer:{
+      type:Boolean,
+      required:true,
+    }
+  },
+  name: "SidebarVue",
+  computed: {
+    getDrawer() {
+      return this.drawer;
+    },
+  },
+};
+</script>
+<style></style>
