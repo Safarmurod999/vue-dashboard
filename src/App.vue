5f10605ff596425eadf2b57b588ac2f73d10907b
Safarmurod999 <supermen0904@gmail.com>
2024-01-12 18:27:51 +0500

    main

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,167 @@
 <template>
   <v-app id="inspire">
-    <SidebarVue :drawer="drawer" />
+    <v-navigation-drawer v-model="drawer" app>
+      <v-sheet color="grey-lighten-4" class="pa-4 text-center">
+        <v-avatar class="mb-4" color="grey-darken-1" size="64"></v-avatar>
+        <div>Dashboard</div>
+      </v-sheet>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item
+          v-for="[icon, text] in links"
+          :key="icon"
+          :prepend-icon="icon"
+          :title="text"
+          :href="text.toLowerCase()=='dashboard'?'/':text.toLowerCase()"
+          link="true"
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
-      <HeaderVue @handleDrawer="drawer = !drawer" />
+      <!-- <HeaderVue @handleDrawer="drawer = !drawer" /> -->
+      <v-app-bar scroll-behavior="elevate">
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-spacer />
+        <v-col lg="4" cols="4" style="border: none">
+          <v-form style="border: none">
+            <v-text-field
+              :loading="loading"
+              variant="outlined"
+              density="compact"
+              hide-details="auto"
+              append-inner-icon="mdi-magnify"
+              clearable
+              single-line
+              @click:append-inner="onClick"
+              label="Search..."
+            />
+          </v-form>
+        </v-col>
+        <v-spacer />
+        <v-menu transition="slide-y-transition">
+          <template v-slot:activator="{ props }">
+            <v-btn v-bind="props"
+              ><v-badge content="5" color="error"
+                ><v-icon size="25">mdi-bell-outline</v-icon></v-badge
+              >
+            </v-btn>
+          </template>
+          <v-list :items="items" item-props lines="three" max-width="350">
+            <template v-slot:subtitle="{ subtitle }">
+              <div v-html="subtitle"></div>
+            </template>
+          </v-list>
+        </v-menu>
+        <v-menu transition="slide-y-transition">
+          <template v-slot:activator="{ props }">
+            <v-btn v-bind="props">
+              <v-avatar size="35">
+                <v-img
+                  cover
+                  src="@/assets/myavatar.jpg"
+                  alt="Safarmurod"
+                  style="width: 100%; border-radius: 50%"
+                />
+              </v-avatar>
+              <span>Safarmurod</span>
+            </v-btn>
+          </template>
+          <v-list width="300">
+            <v-list-item
+              lines="two"
+              prepend-avatar="@/assets/myavatar.jpg"
+              title="Safarmurod O'rinov"
+              subtitle="supermen0904@gmail.com"
+            >
+            </v-list-item>
+            <v-list-item
+              v-for="(menu, index) in menus"
+              :key="index"
+              :prepend-icon="menu.icon"
+              :title="menu.title"
+              :value="menu.title"
+            >
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar>
       <v-container class="py-8 px-6" fluid>
-        <v-row>
-          <v-col v-for="card in cards" :key="card" cols="12">
-            <v-card>
-              <v-list lines="two">
-                <v-list-subheader :title="card"></v-list-subheader>
-
-                <template v-for="n in 6" :key="n">
-                  <v-list-item>
-                    <template v-slot:prepend>
-                      <v-avatar color="grey-darken-1"></v-avatar>
-                    </template>
-
-                    <v-list-item-title
-                      :title="`Message ${n}`"
-                    ></v-list-item-title>
-
-                    <v-list-item-subtitle
-                      title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique"
-                    ></v-list-item-subtitle>
-                  </v-list-item>
-
-                  <v-divider
-                    v-if="n !== 6"
-                    :key="`divider-${n}`"
-                    inset
-                  ></v-divider>
-                </template>
-              </v-list>
-            </v-card>
-          </v-col>
-        </v-row>
+        <router-view></router-view>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-import HeaderVue from "./components/HeaderVue.vue";
-import SidebarVue from "./components/SidebarVue.vue";
-const cards = ["Today", "Yesterday"];
+// import HeaderVue from "./components/HeaderVue.vue";
+
+const links = [
+  ["mdi-view-dashboard", "Dashboard"],
+  ["mdi-account", "Profile"],
+  ["mdi-clipboard-list-outline", "Products"],
+  ["mdi-clipboard-list-outline", "Order"],
+  ["mdi-cog", "Settings"],
+];
 </script>
 
 <script>
 export default {
   data() {
     return {
-      drawer: false,
+      drawer: true,
+      loaded: false,
+      loading: false,
+      menus: [
+        { title: "Profile", icon: "mdi-account" },
+        { title: "Passwords", icon: "mdi-key-outline" },
+        { title: "Settings", icon: "mdi-cog" },
+      ],
+      items: [
+        { type: "subheader", title: "Today" },
+        {
+          prependAvatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
+          title: "Brunch this weekend?",
+          subtitle: `<span class="text-primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        },
+        { type: "divider", inset: true },
+        {
+          prependAvatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+          title: "Summer BBQ",
+          subtitle: `<span class="text-primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+        },
+        { type: "divider", inset: true },
+        {
+          prependAvatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+          title: "Oui oui",
+          subtitle:
+            '<span class="text-primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+        },
+        { type: "divider", inset: true },
+        {
+          prependAvatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
+          title: "Birthday gift",
+          subtitle:
+            '<span class="text-primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+        },
+        { type: "divider", inset: true },
+        {
+          prependAvatar: "https://cdn.vuetifyjs.com/images/lists/5.jpg",
+          title: "Recipe to try",
+          subtitle:
+            '<span class="text-primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+        },
+      ],
     };
+  },
+  methods: {
+    onClick() {
+      this.loading = true;
+
+      setTimeout(() => {
+        this.loading = false;
+        this.loaded = true;
+      }, 2000);
+    },
   },
 };
 </script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="dashboard">
+    <v-subheader class="d-flex justify-space-between align-center">
+      <h3>Dashboard</h3>
+      <v-btn color="success">View Orders</v-btn>
+    </v-subheader>
+    <v-row>
+      <v-col lg="7" cols="12"
+        ><v-alert text="" variant="outlined" type="success" class="my-5">
+          <strong>Login Successfully</strong></v-alert
+        >
+        <v-row>
+          <v-col
+            lg="6"
+            cols="12"
+            v-for="(item, index) in activities"
+            :key="index"
+          >
+            <v-card elevation="2" class="rounded-lg">
+              <v-card-text class="d-flex justify-space-between align-center">
+                <div class="">
+                  <strong>{{ item.title }}</strong
+                  ><br />
+                  <small>Last 2 weeks</small>
+                </div>
+                <v-avatar :color="item.color" size="60">
+                  <span class="white--text">{{ item.amount }}</span>
+                </v-avatar>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col lg="5" cols="12" class="my-5">
+        <v-card title="Activities" class="rounded-lg" elevation="2">
+          <v-timeline
+            side="end"
+            align="start"
+            style="border-top: 1px solid lightgray"
+          >
+            <v-timeline-item dot-color="pink" size="small">
+              <div class="d-flex">
+                <strong class="me-4">5pm</strong>
+                <div>
+                  <strong>New Icon</strong>
+                  <div class="text-caption">Mobile App</div>
+                </div>
+              </div>
+            </v-timeline-item>
+
+            <v-timeline-item dot-color="teal-lighten-3" size="small">
+              <div class="d-flex">
+                <strong class="me-4">3-4pm</strong>
+                <div>
+                  <strong>Design Stand Up</strong>
+                  <div class="text-caption mb-2">Hangouts</div>
+                </div>
+              </div>
+            </v-timeline-item>
+
+            <v-timeline-item dot-color="pink" size="small">
+              <div class="d-flex">
+                <strong class="me-4">12pm</strong>
+                <div>
+                  <strong>Lunch break</strong>
+                  <div class="text-caption">Mobile App</div>
+                </div>
+              </div>
+            </v-timeline-item>
+          </v-timeline></v-card
+        >
+      </v-col>
+    </v-row>
+    <v-row>
+    <v-col cols="12"> 
+       <v-table style="border: 1px solid lightgray;border-radius: 10px;" class="px-3 py-3">
+        <caption style="font-size: 25px;">Recent Orders</caption>
+        <thead>
+          <tr>
+            <th class="text-left">Name</th>
+            <th class="text-left">Calories</th>
+            <th class="text-left">Price</th>
+            <th class="text-left">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in desserts" :key="item.name">
+            <td>{{ item.name }}</td>
+            <td>{{ item.calories }}</td>
+            <td>{{ item.price }}</td>
+            <td><v-btn variant="outlined" color="green">View</v-btn></td>
+          </tr>
+        </tbody>
+      </v-table>
+      <v-row justify="center">
+        <v-col cols="12">
+            <v-pagination
+              v-model="page"
+              density="compact"
+              class="my-2"
+              :length="7"
+            ></v-pagination>
+        </v-col>
+      </v-row>
+    </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  data() {
+    return {
+      activities: [
+        { title: "Total Products", color: "red", amount: 4545 },
+        { title: "Total Categories", color: "green", amount: 25 },
+        { title: "Total Orders", color: "blue", amount: 125 },
+        { title: "Pending Orders", color: "orange", amount: 1245 },
+      ],
+      desserts: [
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+          price:1200
+        },
+        {
+          name: "Ice cream sandwich",
+          calories: 237,
+          price:1200
+        },
+        {
+          name: "Eclair",
+          calories: 262,
+          price:1200
+        },
+        {
+          name: "Cupcake",
+          calories: 305,
+          price:1200
+        },
+        {
+          name: "Gingerbread",
+          calories: 356,
+          price:1200
+        },
+      ],
+      page:1
+    };
+  },
+};
+</script>
+<style></style>
